3acdd835bebe7c5cec2d98a6ab529b0fdaa53656
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-10-30 09:53:35 -0700

    urur

--- helloWorld.js
+++ helloWorld.js
@@ -1,5 +1,9 @@
 const canvas = document.getElementById('glcanvas');
 
+const initShader = (gl, vsShader, fsShader)=>{
+
+}
+
 const main = ()=>{
     const gl = canvas.getContext('webgl');
 
@@ -7,6 +11,16 @@ const main = ()=>{
         alert("Unable to initialize WebGL. Your browser or machine may not support it.");
         return;
     }
+
+    initShader(gl, vsSource, fsShader)
+
+    const loadShader = (gl, type, source)=>{
+        const shader = gl.creeateShader(type);
+        
+    }
+    
+
+    gl.shaderSource(shader, )
 
     gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);

bc2ab0cbe0e33268987bf4568f79e55098eeb937
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-11-05 12:21:13 -0700

    ursus shock

--- helloWorld.js
+++ helloWorld.js
@@ -1,8 +1,74 @@
 const canvas = document.getElementById('glcanvas');
 
-const initShader = (gl, vsShader, fsShader)=>{
-
+const vsSource = `
+attribute vec4 aVertexPosition;
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+void main() {
+  gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
 }
+`;
+
+const fsSource = `
+void main() {
+    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+`;
+
+const loadShader = (gl, type, source)=>{
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        alert('An error occurred compiling the shaders: ' + gl.getShaderInfoLog(shader));
+        gl.deleteShader(shader);
+        return null;
+    }
+
+    return shader;
+}
+
+const initShader = (gl, vsSource, fsSource)=>{
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
+    const shaderProgram = gl.createProgram();
+
+    gl.attachShader(shaderProgram , vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+        alert('Unable to initialize the shader program: ' + gl.getProgramInfoLog(shaderProgram));
+        return null;
+      }
+
+      return shaderProgram;
+}
+
+const initBuffers = gl =>{
+
+    const positionBuffer = gl.createBuffer();
+
+    gl.bindBuffer(gl.ARRAY_BUFFER , positionBuffer);
+
+    const positions = [
+        -1.0,  1.0,
+         1.0,  1.0,
+        -1.0, -1.0,
+         1.0, -1.0,
+      ];
+
+    gl.bufferData(gl.ARRAY_BUFFER,
+        new Float32Array(positions),
+        gl.STATIC_DRAW);
+
+    return {
+        position: positionBuffer
+    };
+}
+
+const drawScene = gl
 
 const main = ()=>{
     const gl = canvas.getContext('webgl');
@@ -12,15 +78,20 @@ const main = ()=>{
         return;
     }
 
-    initShader(gl, vsSource, fsShader)
+    const shaderProgram = initShader(gl, vsSource, fsSource);
 
-    const loadShader = (gl, type, source)=>{
-        const shader = gl.creeateShader(type);
-        
-    }
-    
+    const programInfo = {
+        program: shaderProgram,
+        attribLocations: {
+            VertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition')
+        },
+        uniformLocations: {
+            projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+            modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix')
+        },
+    };
 
-    gl.shaderSource(shader, )
+    const buffers = initBuffers(gl);
 
     gl.clearColor(0, 0, 0, 1);
     gl.clear(gl.COLOR_BUFFER_BIT);
